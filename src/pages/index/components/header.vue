95929e60eeb109cac97f991dfda48cb179d1f368
steven <steven@nenyah.com>
2020-11-24 10:30:05 +0800

    style(header): adjust style

--- src/pages/index/components/header.vue
+++ src/pages/index/components/header.vue
@@ -9,8 +9,7 @@
     <view
         class="flex w-full text-gray-500 bg-white px-20 py-30 items-center justify-between text-2lg border-0 border-b-2 border-gray-500 border-solid"
     >
-        <view class="flex items-center flex-1">
-            <!-- <view class="fa fa-clock-o mx-1 fa-fw" aria-hidden="true"></view> -->
+        <view class="flex items-center flex-shrink">
             <view class="fa-stack text-lg mr-10">
                 <view class="fa fa-circle fa-stack-2x"></view>
                 <view class="fa fa-chevron-down fa-stack-1x fa-rotate-270 fa-inverse"></view>
@@ -18,12 +17,11 @@
             <view class="">{{ today }}</view>
         </view>
         <view class="flex items-center ml-20 flex-1">
-            <!-- <view class="fa fa-sitemap mx-1 fa-fw" aria-hidden="true"></view> -->
             <view class="fa-stack text-lg mr-10">
                 <view class="fa fa-circle fa-stack-2x"></view>
                 <view class="fa fa-sitemap fa-stack-1x fa-inverse"></view>
             </view>
-            <view class="w-10_12 truncate">当前企业：{{ company }}</view>
+            <view class="w-260 truncate">当前企业：{{ company }}</view>
         </view>
     </view>
 </template>
@@ -33,7 +31,7 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class Header extends Vue {
-    private company = '宁波华东医药有限公司'
+    private company = '华东宁波医药有限公司'
     private today = new Date().toLocaleDateString()
 }
 </script>
