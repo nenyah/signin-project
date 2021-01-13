0f3e8d46f4981c4a9e90f91462d1587c8ddeeec0
steven <steven@nenyah.com>
2021-01-13 11:03:40 +0800

    feat(*): add whether to get address logic

--- src/pages/index/components/signin.vue
+++ src/pages/index/components/signin.vue
@@ -6,74 +6,81 @@
  * @LastEditTime: 2020-10-29 14:28:45
 -->
 <template>
-  <view
-    class="w-full text-center flex-auto flex flex-col justify-around box-border bg-helper-gray"
-  >
     <view
-      class="w-full flex flex-col justify-center items-center"
-      style="height:40vh;"
+        class="w-full text-center flex-auto flex flex-col justify-around box-border bg-helper-gray"
     >
-      <view
-        class="rounded-full w-260 h-260 circle flex flex-col justify-center items-center mb-20"
-        style="background:#f8b55e;"
-        @tap="goSubmit"
-      >
-        <view class="text-gray-300 text-3xl">签到</view>
-        <view class="text-white text-5xl">{{ ctime }}</view>
-      </view>
-      <view v-if="checkTimes > 0" class="text-gray-700 text-3xl">
         <view
-          class="fa fa-check-circle-o text-green-500 mx-1"
-          aria-hidden="true"
+            class="w-full flex flex-col justify-center items-center"
+            style="height:40vh;"
         >
+            <view
+                class="rounded-full w-260 h-260 circle flex flex-col justify-center items-center mb-20"
+                style="background:#f8b55e;"
+                @tap="goSubmit"
+            >
+                <view class="text-gray-300 text-3xl">签到</view>
+                <view class="text-white text-5xl">{{ ctime }}</view>
+            </view>
+            <view v-if="checkTimes > 0" class="text-gray-700 text-3xl">
+                <view
+                    class="fa fa-check-circle-o text-green-500 mx-1"
+                    aria-hidden="true"
+                >
+                </view>
+                今日你已签到
+                <text class="text-orange-900">{{ checkTimes }}</text>
+                次
+            </view>
+            <view v-else class="text-gray-700 text-3xl">今日你还没有签到</view>
         </view>
-        今日你已签到
-        <text class="text-orange-900">{{ checkTimes }}</text>
-        次
-      </view>
-      <view v-else class="text-gray-700 text-3xl">今日你还没有签到</view>
+        <view class="text-gray-500">
+            钉钉只会在你主动签到时获取位置信息
+        </view>
     </view>
-    <view class="text-gray-500">
-      钉钉只会在你主动签到时获取位置信息
-    </view>
-  </view>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import {Component, Vue} from 'vue-property-decorator'
 
 @Component({})
 export default class Signin extends Vue {
-  $store: any
+    $store: any
 
-  get ctime() {
-    return this.$store.state.user.ctime.format('HH:mm')
-  }
-
-  get customer() {
-    return this.$store.state.customer.org
-  }
-
-  get user() {
-    return this.$store.state.user.info
-  }
-
-  get checkTimes() {
-    const signinRecordToday = this.$store.state.signin.signinRecordToday
-    return signinRecordToday.userSignCount || 0
-  }
-
-  private goSubmit() {
-    // 1. 判断要不要选择客户
-    // 2. 要选择客户判断有没有选择
-    if (this.user.selectOrg && !this.customer.name) {
-      uni.showToast({ title: '还没有选择客户哦！' })
-      return
+    get ctime() {
+        return this.$store.state.user.ctime.format('HH:mm')
     }
-    uni.navigateTo({
-      url: '/pages/submit/submit',
-    })
-  }
+
+    get customer() {
+        return this.$store.state.customer.org
+    }
+
+    get user() {
+        return this.$store.state.user.info
+    }
+
+    get checkTimes() {
+        const signinRecordToday = this.$store.state.signin.signinRecordToday
+        return signinRecordToday.userSignCount || 0
+    }
+
+
+    private goSubmit() {
+        const address = this.$store.getters.address
+        //  判断地址定位有没有获取成功
+        if (!address) {
+            uni.showToast({title: `还没有地址信息哦！`})
+            return
+        }
+        // 1. 判断要不要选择客户
+        // 2. 要选择客户判断有没有选择
+        if (this.user.selectOrg && !this.customer.name) {
+            uni.showToast({title: '还没有选择客户哦！'})
+            return
+        }
+        uni.navigateTo({
+            url: '/pages/submit/submit',
+        })
+    }
 }
 </script>
 
